e5fb3f10fca73f4d17cec145788fcc2e94151a68
Thural Musaibov <tural.musaibov@gmail.com>
2021-09-25 22:14:15 +0400

    reduced code lines

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
+////////////////////////////NO FRAMEWORK AND NO ENUM FUNCTION means LOTS of CODE ///////////////////////
 //global variables
-let oprContent = '',
-    opr = '';
-let num1 = [],
-    num2 = [];
-let preRender = [];
-let calculated = 0;
-let equalIsOn = false;
+let oprContent = '', opr = '', num1 = [], num2 = [], preRender = [], calculated = 0, equalIsOn = false;
 //object to contain math functions
 const operatorFunctions = {
-    'add': function(a, b) {
-        calculated = a + b
-    },
-    'subtract': function(a, b) {
-        calculated = a - b
-    },
-    'multiply': function(a, b) {
-        calculated = a * b
-    },
-    'divide': function(a, b) {
-        calculated = a / b
-    },
-    'modulus': function(a, b) {
-        calculated = a % b
-    }
+    'add': (a, b) => calculated = a + b,
+    'subtract': (a, b) => calculated = a - b,
+    'multiply': (a, b) => calculated = a * b,
+    'divide': (a, b) => calculated = a / b,
+    'modulus': (a, b) => calculated = a % b
 };
 // display areas
 const disp1 = document.querySelector('#current');
 const disp2 = document.querySelector('#temporal');
 
-//updates content of the smaller display
+//function to update content of the smaller display
 function render1(oprSymbol) {
     if (!(disp1.textContent == '')) num1 = [];
     preRender = (disp1.textContent + num1.join('') +
@@ -42,14 +27,14 @@ function render1(oprSymbol) {
         disp1.textContent = preRender.join('');
     }
 };
-//updates content of the bigger display
+//function to update content of the bigger display
 function render2(oprSymbol) {
     if (!oprSymbol) oprSymbol = oprContent;
     preRender = (num1.join('') + oprSymbol + num2.join('')).split('');
-    if (preRender.length < 15) {
+    if (preRender.length < 14) {
         disp2.textContent = preRender.join('');
     } else {
-        while (preRender.length > 14) {
+        while (preRender.length > 13) {
             preRender.shift();
         };
         disp2.textContent = preRender.join('')
@@ -62,26 +47,16 @@ const calculate = function(operator) {
         !(opr == '')) {
         operatorFunctions[opr](Number(num1.join('')), Number(num2.join('')));
         render1(oprContent);
-        num1 = String(calculated).split('');
-        num2 = [];
-        render2(operator.textContent);
-        opr = '';
+        num1 = String(calculated).split(''), num2 = [];
+        render2(operator.textContent), opr = '';
     };
-    if (!(num1[0] == null)) {
-        opr = operator.id;
-        oprContent = operator.textContent
-    };
-    logg()
+    if (!(num1[0] == null)) opr = operator.id, oprContent = operator.textContent
 };
 //function to insert selected number
 const insert = function(number) {
-    if (!opr) {
-        if (num1.length < 14) num1.push(number.textContent);
-    } else {
-        if (num2.length < 14) num2.push(number.textContent)
-    };
-    render2();
-    logg()
+    if (!opr && num1.length < 14) num1.push(number.textContent);
+        else if (num2.length < 14) num2.push(number.textContent);
+    render2()
 };
 //Object to cotain functions for special buttons
 const functions = {
@@ -99,8 +74,7 @@ const functions = {
         else if (oprContent) opr = '', oprContent = ''
         else num1.pop();
         render2();
-        if (num1[0] == null) disp2.textContent = '0';
-        logg()
+        if (num1[0] == null) disp2.textContent = '0'
     },
     'equals': function() {
         if (!(opr == '') && !(num2[0] == null)) {
@@ -108,39 +82,35 @@ const functions = {
                 Number(num2.join('')));
             (num2[0] == null) ? render1(''): render1(oprContent);
             preRender = String(calculated).split('');
-            if (preRender.length < 14) disp2.textContent = '= ' + preRender.join('')
+            if (preRender.length < 14) disp2.textContent = '= '+preRender.join('')
             else disp2.textContent = '= ' + calculated.toExponential(6);
-            num1 = [];
-            num2 = [];
-            oprContent = '';
-            opr = '';
-            equalIsOn = true;
-            logg();
+            num1 = [], num2 = [];
+            oprContent = '', opr = '';
+            equalIsOn = true
         }
     },
     'clear': function() {
-        disp1.textContent = '';
-        disp2.textContent = '';
-        num2 = [];
-        num1 = [];
-        calculated = 0;
-        oprContent = '';
-        opr = '';
-        render1('')
-        logg()
+        disp1.textContent = '', disp2.textContent = '';
+        num1 = [], num2 = [];
+        calculated = 0, oprContent = '';
+        opr = '', render1('')
     }
 };
-//clear display1 if any button is pressed after equals button clicked
+//click event listener for all buttons
 const allBtns = document.querySelector('#buttons');
+allBtns.addEventListener('click', ()=>{
+  if (event.target.className == 'numbers') insert(event.target)
+    else if (event.target.className == 'operators') calculate(event.target)
+      else if (event.target.className == 'specials') functions[`${event.target.id}`]()
+});
+//change button style on mouse click, also clear display if equal button is pressed
 allBtns.addEventListener('mousedown', (event) => {
     if (!(event.target.id == 'buttons')) event.target.classList.add('clicked');
     if (equalIsOn && !(event.target.id == 'equals')) {
-        disp1.textContent = '', disp2.textContent = '0', equalIsOn = false
-    }
-});
+        disp1.textContent = '', disp2.textContent = '0', equalIsOn = false }});
 allBtns.addEventListener('mouseup', () => event.target.classList.remove('clicked'));
 
-//listener for key events
+//listener for keyboard events
 document.addEventListener('keydown', (event) => {
     const selectedKey = allBtns.querySelector(`div[data-key='${event.key}']`);
     if (!selectedKey) return;
@@ -149,55 +119,17 @@ document.addEventListener('keydown', (event) => {
     };
     if (selectedKey.className == 'operators') calculate(selectedKey)
     else if (selectedKey.className == 'numbers') insert(selectedKey)
-    else if (selectedKey.className == 'specials') {
-        functions[selectedKey.id]();
-    }
+    else if (selectedKey.className == 'specials') functions[selectedKey.id]()
 });
-//click listeners for operator buttons node list
-const oprBtns = Array.from(document.querySelectorAll('.operators'));
-oprBtns.forEach((operator) => {
-    operator.addEventListener('click', () => calculate(operator));
-});
-//click listeners for number buttons node list
-const numBtns = Array.from(document.querySelectorAll('.numbers'));
-numBtns.forEach((number) => {
-    number.addEventListener('click', () => insert(number));
-});
-//click listener for clear button
-const clearBtn = document.getElementById('clear');
-clearBtn.addEventListener('click', () => functions['clear']());
-//click listener for dot button
-const dotBtn = document.getElementById('dot');
-dotBtn.addEventListener('click', () => functions['dot']());
-//click listener for backspace button
-const backspaceBtn = document.getElementById('backspace');
-backspaceBtn.addEventListener('click', () => functions['backspace']());
-//click listener for equals button
-const equalsBtn = document.getElementById('equals');
-equalsBtn.addEventListener('click', () => functions['equals']());
-
 //event listeners to change social media icons on mouse events
 const social = document.querySelector('social');
 social.addEventListener('mouseover', () => {
-    if (!event.target.id) {
-        return
-    };
+    if (!event.target.id) return;
     selectedIcon = document.querySelector(`img[id = '${event.target.id}']`);
     selectedIcon.src = `images/${event.target.id}_icon.png`;
 });
 social.addEventListener('mouseout', () => {
-    if (!event.target.id) {
-        return
-    };
+    if (!event.target.id) return;
     selectedIcon = document.querySelector(`img[id = '${event.target.id}']`);
     selectedIcon.src = `images/black_${event.target.id}_icon.png`;
 });
-/////////////////For Logging////////////////
-function logg() {
-    console.log('/////////////////////////')
-    console.log('preOperand:', num1);
-    console.log('postOperand:', num2);
-    console.log('calculated:', calculated)
-};
-logg()
-    ////////////////////////////////////////////
